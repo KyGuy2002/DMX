cdfcdaa3df2b1e811473045bf52c631c2943f599
IEatBeans <kylanjaeger@gmail.com>
2025-06-01 18:04:43 -0700

    fully working usb to dmx adapter!

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,34 @@
 #include "DMXUSB.h"
+#include <DmxOutput.h>
+#include <Arduino.h>
 
 #define DMXUSB_BAUDRATE 115200
 
+DmxOutput dmx;
+uint8_t universe[512 + 1];
 
-void myDMXCallback(int universe, char buffer[512]) {
-  for (int index=0; index < 512; index++) { // for each channel, universe starts at 0
-    int channel = index + 1; // channel starts at 0, so index 0 is DMX channel 1 and index 511 is DMX channel 512
-    int value = buffer[index]; // DMX value 0 to 255
-    if (universe == 0 && channel == 1) analogWrite(LED_BUILTIN, value); // LED on channel 1 on universe 0
+
+void myDMXCallback(int univNo, char buffer[512]) {
+
+  Serial1.println("- DMX Callback called");
+
+  digitalWrite(LED_BUILTIN, LOW);
+
+  // Copy the buffer to the universe array, starting at index 1
+  for (int index=0; index < 512; index++) {
+    universe[index + 1] = buffer[index]; // universe starts at index 1, index 0 is start code
   }
+
+  dmx.write(universe, 512);
+
+  // while (dmx.busy())
+  // {
+  //   /* Do nothing while the DMX frame transmits */
+  // }
+
+  // Serial1.println("--- DMX fully transmitted");
+
+  digitalWrite(LED_BUILTIN, HIGH);
 }
 
 DMXUSB myDMXUsb(
@@ -20,9 +40,13 @@ DMXUSB myDMXUsb(
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
   Serial.begin(DMXUSB_BAUDRATE);
+  Serial1.begin(115200); // Serial1 is used for debugging
+  dmx.begin(2);
 }
 
 void loop() {
   myDMXUsb.listen();
+
 }
