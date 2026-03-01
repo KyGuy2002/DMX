f4f174c93e8e36e7d6a072f1677201ec176e1ce6
IEatBeans <kylanjaeger@gmail.com>
2026-02-28 17:08:13 -0800

    WORKING AUDIO FULL SONG PLAY

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,20 @@
 #include <Arduino.h>
 #include "AudioTools.h"
-// #include "audio5.h"
+#include "untitled.h"
 
-AudioInfo info(48000, 2, 16);
+AudioInfo info(16000, 1, 16);
 I2SStream out;
-// MemoryStream sound(audio5_raw, audio5_raw_len);
-SineWaveGenerator<int16_t> sine;
-GeneratedSoundStream<int16_t> sound(sine);
-StreamCopy copier(out, sound); // copies sound into i2s
+MemoryStream music(untitled_raw, untitled_raw_len);
+StreamCopy copier(out, music); // copies sound into i2s
 
 // Arduino Setup
 void setup(void) {  
   // Open Serial 
-
-  sine.begin(info, 440);
+  Serial1.begin(115200);
+  AudioToolsLogger.begin(Serial1, AudioToolsLogLevel::Info);
 
   // start i2s
+  Serial1.println("starting I2S...");
   I2SConfig config = out.defaultConfig(TX_MODE);
   config.copyFrom(info);
   config.buffer_size = 2048;
@@ -26,5 +25,8 @@ void setup(void) {
 
 // Arduino loop - copy sound to out 
 void loop() {
-  copier.copy();
+  if (!copier.copy()){
+      out.end();
+      stop();
+    }
 }
