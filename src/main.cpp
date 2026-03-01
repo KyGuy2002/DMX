1fc03741513a824fbf0d27bc8b6b69b48e7f22a0
IEatBeans <kylanjaeger@gmail.com>
2026-02-28 16:11:02 -0800

    stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,69 +1,30 @@
-#include <Arduino.h>
-#include <SPI.h>
-#include <SD.h>
+#include "AudioTools.h"
 
-File myFile;
+AudioInfo info(48000, 2, 16);
+SineWaveGenerator<int16_t> sineWave(32000);                // subclass of SoundGenerator with max amplitude of 32000
+GeneratedSoundStream<int16_t> sound(sineWave);             // Stream generated from sine wave
+I2SStream out; 
+StreamCopy copier(out, sound);                             // copies sound into i2s
 
-const int SD_CS_PIN = 13; // Pin for SD card chip select
-const int SD_MOSI_PIN = 15; // Pin for SD card MOSI
-const int SD_MISO_PIN = 12; // Pin for SD card MISO
-const int SD_CLK_PIN = 14; // Pin for SD card clock
-
+// Arduino Setup
 void setup(void) {  
-
-  // SPI1.setCS(13);
-  // SPI1.begin();
-  
+  // Open Serial 
   Serial1.begin(115200);
-  delay(1500);
+  while(!Serial1);
+  AudioToolsLogger.begin(Serial1, AudioToolsLogLevel::Info);
 
-  SPI1.setSCK(SD_CLK_PIN);  // CLK
-  SPI1.setTX(SD_MOSI_PIN);  // MOSI
-  SPI1.setRX(SD_MISO_PIN);  // MISO
+  // start I2S
+  Serial1.println("starting I2S...");
+  auto config = out.defaultConfig(TX_MODE);
+  config.copyFrom(info); 
+  out.begin(config);
 
-
-  if (!SD.begin(SD_CS_PIN, SPI1)) {  // CS
-    Serial1.println("SD init failed!");
-    while (1) {
-      Serial1.println("SD init failed!");
-      delay(1000);  // wait for a second
-    }
-  }
-  Serial1.println("initialization done.");
-
-  // open the file. note that only one file can be open at a time,
-  // so you have to close this one before opening another.
-  myFile = SD.open("test.txt", FILE_WRITE);
-
-  // if the file opened okay, write to it:
-  if (myFile) {
-    Serial1.print("Writing to test.txt...");
-    myFile.println("testing 1, 2, 3.");
-    // close the file:
-    myFile.close();
-    Serial1.println("done.");
-  } else {
-    // if the file didn't open, print an error:
-    Serial1.println("error opening test.txt");
-  }
-
-  // re-open the file for reading:
-  myFile = SD.open("test.txt");
-  if (myFile) {
-    Serial1.println("test.txt:");
-
-    // read from the file until there's nothing else in it:
-    while (myFile.available()) {
-      Serial1.write(myFile.read());
-    }
-    // close the file:
-    myFile.close();
-  } else {
-    // if the file didn't open, print an error:
-    Serial1.println("error opening test.txt");
-  }
-
+  // Setup sine wave
+  sineWave.begin(info, N_B4);
+  Serial1.println("started..."); 
 }
 
+// Arduino loop - copy sound to out 
 void loop() {
+  copier.copy();
 }
