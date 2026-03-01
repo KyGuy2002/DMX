c69d0e26f49474f4fbaa3a7b0087655cacb3af4c
IEatBeans <kylanjaeger@gmail.com>
2026-02-28 16:48:20 -0800

    working.  need buffersize

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,27 @@
+#include <Arduino.h>
 #include "AudioTools.h"
+// #include "audio5.h"
 
 AudioInfo info(48000, 2, 16);
-SineWaveGenerator<int16_t> sineWave(32000);                // subclass of SoundGenerator with max amplitude of 32000
-GeneratedSoundStream<int16_t> sound(sineWave);             // Stream generated from sine wave
-I2SStream out; 
-StreamCopy copier(out, sound);                             // copies sound into i2s
+I2SStream out;
+// MemoryStream sound(audio5_raw, audio5_raw_len);
+SineWaveGenerator<int16_t> sine;
+GeneratedSoundStream<int16_t> sound(sine);
+StreamCopy copier(out, sound); // copies sound into i2s
 
 // Arduino Setup
 void setup(void) {  
   // Open Serial 
-  Serial1.begin(115200);
-  while(!Serial1);
-  AudioToolsLogger.begin(Serial1, AudioToolsLogLevel::Info);
 
-  // start I2S
-  Serial1.println("starting I2S...");
-  auto config = out.defaultConfig(TX_MODE);
-  config.copyFrom(info); 
+  sine.begin(info, 440);
+
+  // start i2s
+  I2SConfig config = out.defaultConfig(TX_MODE);
+  config.copyFrom(info);
+  config.buffer_size = 2048;
   out.begin(config);
 
-  // Setup sine wave
-  sineWave.begin(info, N_B4);
-  Serial1.println("started..."); 
+  delay(50);
 }
 
 // Arduino loop - copy sound to out 
