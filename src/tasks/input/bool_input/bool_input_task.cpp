d108e319dfa47bb307fef02a6a8629efa7acbbaf
IEatBeans <kylanjaeger@gmail.com>
2026-05-09 16:29:37 -0700

    temp analog read

--- src/tasks/input/bool_input/bool_input_task.cpp
+++ src/tasks/input/bool_input/bool_input_task.cpp
@@ -1,20 +1,8 @@
 #include "bool_input_task.h"
 #include "../../../tasks/mdns/mdns_task.h"
 
-// Debounce settings
-static const unsigned long DEBOUNCE_DELAY = 50; // ms
-static volatile unsigned long lastInterruptTime = 0;
-
-// Queue for button press events (using enum for event type)
-enum ButtonEvent {
-    BUTTON_PRESSED = 1
-};
-
-static QueueHandle_t buttonEventQueue = NULL;
 
 void createBoolInputTask() {
-    // Create the event queue before starting the task
-    buttonEventQueue = xQueueCreate(5, sizeof(ButtonEvent));
     
     xTaskCreate(
         boolInputTask,             // Task function
@@ -26,48 +14,25 @@ void createBoolInputTask() {
     );
 }
 
-// Interrupt Service Routine for button press
-void buttonPressISR() {
-    unsigned long currentTime = millis();
-    
-    // Debounce: ignore interrupts within DEBOUNCE_DELAY ms of last interrupt
-    if ((currentTime - lastInterruptTime) > DEBOUNCE_DELAY) {
-        lastInterruptTime = currentTime;
-        
-        // Queue the button press event
-        ButtonEvent event = BUTTON_PRESSED;
-        BaseType_t xHigherPriorityTaskWoken = pdFALSE;
-        xQueueSendFromISR(buttonEventQueue, &event, &xHigherPriorityTaskWoken);
-        
-        // Yield if a higher priority task was woken
-        portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
-    }
-}
-
 
 void boolInputTask(void *pvParameters) {
     // Initialize button pin as input with pull-up
     pinMode(INPUT_1_PIN, INPUT_PULLUP);
     
-    // Attach interrupt to button pin (FALLING = pin goes to GND when pressed)
-    attachInterrupt(digitalPinToInterrupt(INPUT_1_PIN), buttonPressISR, FALLING);
-    
-    Serial1.println("[Bool Input] Task started - interrupt driven");
-    
-    // Wait for button press events from the queue
-    ButtonEvent event;
     while (1) {
-        // Wait indefinitely for an event from the queue
-        if (xQueueReceive(buttonEventQueue, &event, portMAX_DELAY) == pdTRUE) {
-            if (event == BUTTON_PRESSED) {
-                Serial1.println("[Bool Input] INPUT_1_PIN button pressed!");
-                
-                // Spawn a temporary background task for HTTP request
-                // This prevents the button task from blocking other tasks
-                
-                // Button task immediately returns to waiting for next press
-                vTaskDelay(pdMS_TO_TICKS(1));  // Yield to scheduler
-            }
-        }
+
+        // Current ms
+        unsigned long currentMillis = millis();
+
+        // Print the button state to Serial
+        Serial1.print("[");
+        Serial1.print(currentMillis);
+        Serial1.print("] Button State: ");
+        Serial1.println(analogRead(INPUT_1_PIN));
+
+
+        // Delay for a short period to debounce and avoid flooding the serial output
+        vTaskDelay(pdMS_TO_TICKS(100));
+
     }
 }
