0aab26428a34200fe03fe4703334b37e52b1a242
IEatBeans <kylanjaeger@gmail.com>
2026-05-25 21:39:53 -0700

    asdads

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -6,5 +6,6 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <h1 className="text-4xl font-bold">asdasdasd</h1>
   </StrictMode>,
 )
