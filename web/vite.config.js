e8a5a3fefd1fa2c54602a3149da68b7ff01199e3
IEatBeans <kylanjaeger@gmail.com>
2026-05-25 18:25:01 -0700

    start adding web interface

---
+++ web/vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    watch: {
+      // Force the watcher to use polling. 
+      // This is often necessary on Windows or networked filesystems.
+      usePolling: true,
+    },
+  },
+})
